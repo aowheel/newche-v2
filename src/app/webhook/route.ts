0429fd1e9e711694c89acf16a4b502f49a6a8a7a
aowheel <aoimakino2003@gmail.com>
2024-12-05 14:45:16 +0900

    webhookの修正

--- src/app/webhook/route.ts
+++ src/app/webhook/route.ts
@@ -3,7 +3,7 @@ import { upsertGroup } from "@/lib/data";
 import crypto from "crypto";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
+export async function POST(req: Request) {
   const client_secret = process.env.LINE_CLIENT_SECRET || "";
   const body = await req.text();
   const signature = crypto
